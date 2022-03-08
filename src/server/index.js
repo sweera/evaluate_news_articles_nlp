b8ce1234f1f108300cca88457a1ba67e755308d0
sweera <sweerachahal95@gmail.com>
2022-03-07 23:40:31 -0800

    dotenv

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
-// var textapi = new meaningcloud({
-//     application_key: "",
-//   });
+var textapi = new meaningcloud({
+    application_key: process.env.API_KEY
+  });
 
 const app = express()
 
