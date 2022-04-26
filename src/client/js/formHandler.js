86f3e65a9741c674f87cd736dfe8623f1a6b7767
sweera <sweerachahal95@gmail.com>
2022-04-25 23:56:51 -0700

    ui elements

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -35,5 +35,25 @@ function handleSubmit(event) {
     //    // document.getElementById('results').innerHTML = res.message
     //     document.getElementById('results').innerHTML = res.message
     // })
+
+// Function to POST data
+    const postData = async (url = "", data = {}) => {
+        const response = await fetch(url, {
+          method: "POST",
+          credentials: "same-origin",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+        try {
+          const newData = await response.json();
+          console.log(newData);
+          return newData;
+        } catch (error) {
+          console.log("error", error);
+        }
+      };
+      
 Client.checkForName(formText)
 export { handleSubmit }
